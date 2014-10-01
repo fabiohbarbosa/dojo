f22a9de476f86dbfe0427aea93b026477d500743
Fábio H. G. Barbosa <fabio.barbosa@gauge.com.br>
2014-09-30 23:08:36 -0300

    Jogo da vida

--- src/main/java/com/wordpress/fabiohbarbosa/dojo/jogodavida/JogoDaVida.java
+++ src/main/java/com/wordpress/fabiohbarbosa/dojo/jogodavida/JogoDaVida.java
@@ -4,12 +4,78 @@ public class JogoDaVida {
 
 	private boolean[][] tabuleiro;
 	
-	public JogoDaVida(boolean[][] tabuleiro) {
-		this.tabuleiro = tabuleiro;
+	private int totalLinhas;
+	private int totalColunas;
+	
+	public JogoDaVida(int totalLinhas, int totalColunas) {
+		this.totalLinhas = totalLinhas;
+		this.totalColunas = totalColunas;
+		tabuleiro = new boolean[totalLinhas][totalColunas];
 	}
 
-	public boolean verificarSituacaoCelula(int x, int y) {
-		return false;
+	public void adicionarVizinho(int linha, int coluna) {
+		tabuleiro[linha][coluna] = true;
+	}
+	
+	protected boolean verificarCelulaViva(int linha, int coluna) {
+		int vizinhos = retonarQuantidadeDeVizinhos(linha, coluna);
+		
+		// Baixa população
+		if (vizinhos < 2) {
+			return false;
+		}
+		// Alta população
+		if (vizinhos > 3) {
+			return false;
+		}
+		
+		if (vizinhos == 2 || vizinhos == 3) {
+			return true;
+		}
+		
+		throw new RuntimeException();
+		
+	}
+
+	protected int retonarQuantidadeDeVizinhos(int linha, int coluna) {
+		int vizinhos = 0;
+		
+		// Esquerda
+		if (coluna > 0) {
+			if (tabuleiro[linha][coluna - 1]) {
+				vizinhos++;
+			}
+		}
+
+		// Direita
+		if (coluna < totalColunas - 1) {
+			if (tabuleiro[linha][coluna + 1]) {
+				vizinhos++;
+			}
+		}
+		
+		// Acima
+		if (linha > 0) {
+			if (tabuleiro[linha - 1][coluna]) {
+				vizinhos++;
+			}
+		}
+		
+		// Abaixo
+		if (linha < totalLinhas-1) {
+			if (tabuleiro[linha + 1][coluna]) {
+				vizinhos++;
+			}
+		}
+		
+		return vizinhos;
+	}
+
+	public boolean movimentarCelula(boolean celula, int linha, int coluna) {
+		if (celula == false && retonarQuantidadeDeVizinhos(linha, coluna) == 2) {
+			return false;
+		}
+		return verificarCelulaViva(linha, coluna);
 	}
 
 }
--- src/test/java/com/wordpress/fabiohbarbosa/dojo/jogodavida/JogoDaVidaTest.java
+++ src/test/java/com/wordpress/fabiohbarbosa/dojo/jogodavida/JogoDaVidaTest.java
@@ -1,35 +1,214 @@
 package com.wordpress.fabiohbarbosa.dojo.jogodavida;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
-import org.junit.Before;
 import org.junit.Test;
 
 public class JogoDaVidaTest {
-	private boolean[][] tabuleiro;
+	private final int totalLinhas = 10;
+	private final int totalColunas = 5;
+	
 	private JogoDaVida jogoDaVida;
 	
-	@Before
-	public void setUp() {
-		tabuleiro = new boolean[10][10];
-		jogoDaVida = new JogoDaVida(tabuleiro);
+	public JogoDaVidaTest() {
+		jogoDaVida = new JogoDaVida(totalLinhas, totalColunas);
 	}
 	
 	@Test
-	public void celulaDeveEstarMortaPorBaixaPopulacaoComMenosDeDoisVizinhos() {
-		int x = 0;
-		int y = 0;
+	public void celulaVivaDeveMorrerPorBaixaPopulacao() {
+		int linha = 0;
+		int coluna = 1;
 		
-		boolean viva = jogoDaVida.verificarSituacaoCelula(x, y);
+		jogoDaVida.adicionarVizinho(0, 0);
+		
+		boolean viva = jogoDaVida.movimentarCelula(true, linha, coluna);
 		assertFalse(viva);
 	}
 	
 	@Test
-	public void celulaDeveEstarMortaPorAltaPopulacaoComMaisDeTresVizinhos() {
-		int x = 0;
-		int y = 0;
+	public void celulaVivaDeveMorrerPorAltaPopulacao() {
+		int linha = 1;
+		int coluna = 1;
 		
-		boolean viva = jogoDaVida.verificarSituacaoCelula(x, y);
+		jogoDaVida.adicionarVizinho(1, 0);
+		jogoDaVida.adicionarVizinho(1, 2);
+		jogoDaVida.adicionarVizinho(0, 1);
+		jogoDaVida.adicionarVizinho(2, 1);
+		
+		boolean viva = jogoDaVida.movimentarCelula(true, linha, coluna);
 		assertFalse(viva);
+	}
+	
+	@Test
+	public void celulaVivaDevePermanecerVivaParaProximaGeracao() {
+		int linha = 1;
+		int coluna = 1;
+		
+		jogoDaVida.adicionarVizinho(1, 0);
+		jogoDaVida.adicionarVizinho(1, 2);
+		
+		boolean viva = jogoDaVida.movimentarCelula(true, linha, coluna);
+		assertTrue(viva);
+	}
+	
+	@Test
+	public void celulaMortaDeveSeTransformarEmCelulaVivaComTresCelulasVizinhas() {
+		int linha = 1;
+		int coluna = 1;
+		
+		jogoDaVida.adicionarVizinho(1, 0);
+		jogoDaVida.adicionarVizinho(1, 2);
+		jogoDaVida.adicionarVizinho(0, 1);
+		
+		boolean viva = jogoDaVida.movimentarCelula(false, linha, coluna);
+		assertTrue(viva);
+	}
+	
+	@Test
+	public void celulaVivaSemNenhumVizinhoDeveMorrer() {
+		int linha = 1;
+		int coluna = 1;
+		
+		boolean viva = jogoDaVida.movimentarCelula(true, linha, coluna);
+		assertFalse(viva);
+	}
+	
+	@Test
+	public void celulaMortaDevePermanecerMortaComDuasCelulasVizinhas() {
+		int linha = 1;
+		int coluna = 1;
+		
+		jogoDaVida.adicionarVizinho(1, 0);
+		jogoDaVida.adicionarVizinho(1, 2);
+		
+		boolean viva = jogoDaVida.movimentarCelula(false, linha, coluna);
+		assertFalse(viva);
+	}
+	
+	@Test
+	public void celulaDeveEstarMortaUmVizinhoADireita() {
+		int linha = 0;
+		int coluna = 0;
+
+		jogoDaVida.adicionarVizinho(0, 1);
+		
+		boolean viva = jogoDaVida.verificarCelulaViva(linha, coluna);
+		assertFalse(viva);
+	}
+	
+	@Test
+	public void celulaDeveEstarMortaComQuatroVizinhos() {
+		int linha = 2;
+		int coluna = 2;
+		
+		jogoDaVida.adicionarVizinho(1, 2);
+		jogoDaVida.adicionarVizinho(2, 1);
+		jogoDaVida.adicionarVizinho(2, 3);
+		jogoDaVida.adicionarVizinho(3, 2);
+		
+		boolean viva = jogoDaVida.verificarCelulaViva(linha, coluna);
+		assertFalse(viva);
+	}
+	
+	@Test
+	public void celulaDeveEstarVivaComDoisVizinhosNasLaterais() {
+		int linha = 3;
+		int coluna = 3;
+		
+		jogoDaVida.adicionarVizinho(3, 2);
+		jogoDaVida.adicionarVizinho(3, 4);
+		
+		boolean viva = jogoDaVida.verificarCelulaViva(linha, coluna);
+		assertTrue(viva);
+	}
+	
+	@Test
+	public void celulaDeveEstarMortaParaPosicao21ComQuatroVizinhos() {
+		int linha = 2;
+		int coluna = 1;
+		
+		jogoDaVida.adicionarVizinho(1, 1);
+		jogoDaVida.adicionarVizinho(2, 0);
+		jogoDaVida.adicionarVizinho(2, 2);
+		jogoDaVida.adicionarVizinho(3, 1);
+		
+		boolean viva = jogoDaVida.verificarCelulaViva(linha, coluna);
+		assertFalse(viva);
+	}
+	
+	@Test
+	public void deveRetornarDoisVizinhos() {
+		int linha = 3;
+		int coluna = 3;
+		
+		jogoDaVida.adicionarVizinho(3, 2);
+		jogoDaVida.adicionarVizinho(3, 4);
+		
+		int vizinhos = jogoDaVida.retonarQuantidadeDeVizinhos(linha, coluna);
+		assertEquals(2, vizinhos);
+	}	
+	
+	@Test
+	public void deveRetornarQuatroVizinhos() {
+		int linha = 2;
+		int coluna = 2;
+		
+		jogoDaVida.adicionarVizinho(1, 2);
+		jogoDaVida.adicionarVizinho(2, 1);
+		jogoDaVida.adicionarVizinho(2, 3);
+		jogoDaVida.adicionarVizinho(3, 2);
+				
+		int vizinhos = jogoDaVida.retonarQuantidadeDeVizinhos(linha, coluna);
+		assertEquals(4, vizinhos);
+	}
+	
+	@Test
+	public void deveRetornarDoisVizinhosParaPrimeiraColunaPrimeiraLinha() {
+		int linha = 0;
+		int coluna = 0;
+		
+		jogoDaVida.adicionarVizinho(0, 1);
+		jogoDaVida.adicionarVizinho(1, 0);
+		
+		int vizinhos = jogoDaVida.retonarQuantidadeDeVizinhos(linha, coluna);
+		assertEquals(2, vizinhos);
+	}
+	
+	@Test
+	public void deveRetornarDoisVizinhosParaPrimeiraColunaUltimaLinha() {
+		int linha = 0;
+		int coluna = 4;
+		
+		jogoDaVida.adicionarVizinho(0, 3);
+		jogoDaVida.adicionarVizinho(1, 4);
+		
+		int vizinhos = jogoDaVida.retonarQuantidadeDeVizinhos(linha, coluna);
+		assertEquals(2, vizinhos);
+	}
+	
+	@Test
+	public void deveRetornarDoisVizinhosParaUltimaLinhaUltimaColuna() {
+		int linha = 9;
+		int coluna = 4;
+		
+		jogoDaVida.adicionarVizinho(9, 3);
+		jogoDaVida.adicionarVizinho(8, 4);
+		
+		int vizinhos = jogoDaVida.retonarQuantidadeDeVizinhos(linha, coluna);
+		assertEquals(2, vizinhos);
+	}
+	
+	@Test
+	public void deveRetornarDoisVizinhosParaUltimaLinhaPrimeiraColuna() {
+		int linha = 9;
+		int coluna = 0;
+		
+		jogoDaVida.adicionarVizinho(8, 0);
+		jogoDaVida.adicionarVizinho(9, 1);
+		
+		int vizinhos = jogoDaVida.retonarQuantidadeDeVizinhos(linha, coluna);
+		assertEquals(2, vizinhos);
 	}
 }
